4707ca973d32841111bc386a9fe7f6fdec1bf854
totto2727 <h_tsuchida@yumemi.co.jp>
2024-11-13 03:06:41 +0900

    wip

--- app/routes/kinde-auth.$index.tsx
+++ app/routes/kinde-auth.$index.tsx
@@ -2,5 +2,6 @@ import { handleAuth } from "@kinde-oss/kinde-remix-sdk";
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
+	console.log("loader", JSON.stringify(process.env));
 	return await handleAuth(request, params.index);
 }
